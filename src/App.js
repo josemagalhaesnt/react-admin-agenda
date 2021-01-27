095c6b53416297321f8748f2b5251452c332ba9e
José Magalhães <zneto@live.com>
2021-01-27 15:00:43 -0300

    Created UserList as separate component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import * as React from "react";
-import { Admin, Resource, ListGuesser } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
+import * as React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './components/UserList';
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
-  return (
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
-    </Admin>
-  );
+	return (
+		<Admin dataProvider={dataProvider}>
+			<Resource name="users" list={UserList} />
+		</Admin>
+	);
 };
 
 export default App;
